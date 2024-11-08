19e298de170dfc3853b035d5112035d825de642e
XPERIARGLUNA <lnjesusantonio@gmail.com>
2024-11-08 13:13:25 -0600

    Create XperiarGLuna.cs

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/XperiarGLuna.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+// url del sitio oficial de c# https://dotnet.microsoft.com/es-es/languages/csharp
+
+     //Esto es un comentario de una linea.
+
+            /* Y este es
+             * de varias 
+             * lineas */
+
+
+            //Creando mi primer variable 
+            int miPrimerVariable = 0;
+            //Creando mi primer constante
+            const double PI = 3.1416;
+
+            //Tipos de datos primitivos
+            int num1 = 1;
+            double num2 = 2.5;
+            char miCaracter = 'A';
+            string miCadena = "Hola";
+            bool miBoolean = false;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola, C#!");
+        }
+    }
+}
